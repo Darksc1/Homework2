50a365ab81e164399360c53d84cfe9aee8baf24d
klashkintm <klashkintm@mail.ru>
2021-11-12 08:47:49 +0300

    Добавьте файлы проекта.

---
+++ Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            var numbers = Array.ConvertAll(lines, int.Parse);
+            int second = numbers.OrderByDescending(x => x).ElementAt(1);
+            Console.WriteLine(second);
+        }
+    }
+}
